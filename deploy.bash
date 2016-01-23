c54a5310b83787e890b95bf43646b65f2400c935
toshi <me@toshimaru.net>
2016-01-24 02:36:36 +0900

    Output branch name

--- deploy.bash
+++ deploy.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [[ $TRAVIS_BRANCH == "master" ]]; then
+  echo "Deploy to Github Page ($TRAVIS_BRANCH)"
   bundle exec rake autodeploy
 else
-  echo 'no deploy'
+  echo "No deploy ($TRAVIS_BRANCH)"
 fi
